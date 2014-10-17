cff93f4826cd35e23540a3f68df0518e0a764c19
Joseph Matthias Goh <joseph@zephinzer.com>
2014-10-17 14:51:50 +0800

    redirect loop in google login now. fixing it

--- libs/GoogleLogin/index.php
+++ libs/GoogleLogin/index.php
@@ -9,6 +9,8 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/libs/Opauth/Opauth.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/libs/Opauth/OpauthStrategy.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/libs/Opauth/Strategy/Google/GoogleStrategy.php';
 $Opauth = new GoogleStrategy( $config['Strategy']['Google'] , $config);
+var_dump($_SESSION);
+exit;
 if(isset($_SESSION['opauth'])) {
 	//var_dump($_SESSION['opauth']);
 	header('Location: /', 301);
