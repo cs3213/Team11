d1019533fe60002cde33f40e415d7c6d523f0567
Joseph Matthias Goh <joseph@zephinzer.com>
2014-10-17 15:04:23 +0800

    still tryingg.

--- libs/GoogleLogin/index.php
+++ libs/GoogleLogin/index.php
@@ -9,11 +9,6 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/libs/Opauth/Opauth.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/libs/Opauth/OpauthStrategy.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/libs/Opauth/Strategy/Google/GoogleStrategy.php';
 $Opauth = new GoogleStrategy( $config['Strategy']['Google'] , $config);
-var_dump($_SESSION);
-var_dump($_GET);
-var_dump($_POST);
-var_dump($Opauth->auth);
-exit;
 if(isset($_SESSION['opauth'])) {
 	//var_dump($_SESSION['opauth']);
 	header('Location: /', 301);
