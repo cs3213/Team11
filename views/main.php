bdc00a2cacbf3f4adfc9802a9a5c4b7bf963aa19
bubsnow <bubsnow@gmail.com>
2014-10-19 00:33:16 +0800

    Main description: help me check if this works

--- views/main.php
+++ views/main.php
@@ -1,1 +1,26 @@
-main
+<html>
+<head>
+<title>Main</title>
+</head>
+<body>
+<?php
+                $text = '<p><h1 style="color:white">Main<p></h1>';
+                echo ($text);
+                echo "\n";
+?>
+
+<?php
+                $text = '<p style ="color:white">Hello! </br>
+														This is an app design to help you to learn coding!</br>
+														</br>
+														You may first log in with your Google account by pressing the red button at the bottom of this panel.
+														This will allow you to save your current status.
+														Now click on objectives to see the missions available.</br></br>
+														
+														Have fun! :)</p>';
+                echo ($text);
+                echo "\n";
+?>
+
+</body>
+</html>
