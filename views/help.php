f056e414090b41c051d137340766076d54adbe2b
bubsnow <bubsnow@gmail.com>
2014-10-19 01:19:39 +0800

    Added help document

--- views/help.php
+++ views/help.php
@@ -1,1 +1,58 @@
-help
+<html>
+<head>
+<title>Help</title>
+</head>
+<body>
+<?php
+                $text = '<p><h2 style="color:white">Stuck?<p></h2>';
+                echo ($text);
+                echo "\n";
+?>
+
+<?php
+                $text = '<p style ="color:white">
+  													 <ul>
+														 			<li> Logging in
+																	 <p><font size = "2"> Press the red button "Sign in with Google in order to sign in. </br>
+                                   To log out, press the log out button that is below the email address.</font></p></li>
+														 			<li> Drag n Drop Mode
+                                   <p><font size = "2"> 
+                                   <ul>
+                                   <li> To open/close this menue</br>
+                                   Click on the triple cube icon to toggle.</li>
+                                   <li> To run command</br>
+                                   Click and drag instruction on the left into the grey rectangle labeled workspace.</li>
+                                   <li> To move character</br>
+                                   Type in non-negative number into boxes next to label "set x" to shift right, and "set y" to shift up.</br>
+                                   Type a non-negative number in the box next to "move".
+                                   </li>
+                                   <li> To show/hide character</br>
+                                   First drag "hide" into the grey rectangle to make  the pikachu disappear. To make it reappear, drag in command "show" after command "hide".</li>
+                                   <li> Change costume/Background</br>
+                                   Click the drop down menue and select your choice!
+                                   </li>
+                                   <li> Repeat</br>
+                                   To repeat an action a number of times, type a
+                                   non-negative number into the box next to repeat.
+                                   </li>
+                                   <li> Delete Action</br>
+                                   Drag the action from the grey rectangle into the dustbin to delete action.</li>
+                                    <li> Stuck? Character disappeared?</br>
+                                   Press F5</li>
+                                   </ul>
+                                   
+                                   
+                                   </font>
+                                   </p>
+                                   
+                                   </li>
+														 			<li> Viewing changes </br>
+                                  <font size = "2"> Press play to see changes that you have made!</font>
+														 			</li>
+														</p>';
+                echo ($text);
+                echo "\n";
+?>
+
+</body>
+</html>
