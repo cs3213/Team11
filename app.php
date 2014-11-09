8625d6a6b78ac4b59ec94a7915da88192843598d
Kai Yao <yky234-github@yahoo.com.sg>
2014-11-09 20:12:37 +0800

    Use patched touch-punch to support input entry with touch punch.

--- app.php
+++ app.php
@@ -129,6 +129,8 @@
 			</background-instance>
 			<character-instance ng-controller="characterController">
 			</character-instance>
+			<character-instance ng-controller="characterController">
+			</character-instance>
 			
 			<!-- /drawing board -->
 			<div class="navbar navbar-app navbar-absolute-bottom" style="border-radius:0px;">
@@ -149,7 +151,7 @@
 		<script src="/assets/js/angular-ui-sortable.js"></script>
 		<script src="/assets/js/mobile-angular-ui.min.js"></script>
 		<script src="/assets/js/ui-utils.js"></script>
-		<script src="/assets/js/jquery.ui.touch-punch.min.js"></script>
+		<script src="/assets/js/jquery.ui.touch-punch.js"></script>
 		<script src="/app.js"></script>
 		<script src="/app-controllers.js"></script>
 		<script src="/app-directives.js"></script>
